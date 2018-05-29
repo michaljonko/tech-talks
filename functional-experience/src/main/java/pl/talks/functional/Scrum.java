d22b54e5450ecffbde6cc840a39223f155af9cf3
Krzysztof Borowa <Krzysztof_Borowa@epam.com>
2018-05-29 13:37:37 +0200

    initial scrum project concept

---
+++ functional-experience/src/main/java/pl/talks/functional/Scrum.java
@@ -0,0 +1,32 @@
+package pl.talks.functional;
+
+import java.util.Set;
+
+public class Scrum {
+
+    class Grooming {
+        public void make() {
+            // implement grooming class
+            // Steps:
+            // 1. PO provides data relating to the ticket
+            // 2. DEV -> asks questions if there are any
+            // 3. PO -> answers the questions
+            // 4. estimations
+            // 5. write estimations to file connected to ticket
+            // 6. go to next ticket
+        }
+    }
+
+    class PO {
+    }
+
+    class Team {
+        Set<TeamMember> members;
+    }
+
+    class Sprint {
+    }
+
+    class TeamMember {
+    }
+}
