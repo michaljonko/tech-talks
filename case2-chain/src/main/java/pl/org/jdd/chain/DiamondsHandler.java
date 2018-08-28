cb78965cfa3e113dc599a2c8307d0411b6a1b5c0
Michal Jonko <michal.jonko@gmail.com>
2018-08-29 00:49:48 +0200

    simple chain

--- case2-chain/src/main/java/pl/org/jdd/chain/DiamondsHandler.java
+++ case2-chain/src/main/java/pl/org/jdd/chain/DiamondsHandler.java
@@ -1,6 +1,8 @@
 package pl.org.jdd.chain;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.vavr.Function1;
+import io.vavr.collection.List;
 import lombok.extern.slf4j.Slf4j;
 import pl.org.jdd.legacy.Handler;
 import pl.org.jdd.legacy.stub.Treasury;
@@ -27,8 +29,30 @@ public final class DiamondsHandler implements Handler<Diamond> {
     this.validator = validator;
   }
 
+  public static void main(String[] args) {
+    new DiamondsHandler(new Treasury(), new DiamondMessageConverter(), new DiamondValidator(), null)
+        .handleSouvenir(null);
+  }
+
   @Override
   public void handleSouvenir(Diamond diamond) {
+    List<Function1<Diamond, Diamond>> functions = List.of(
+        d -> {
+          System.out.println("1nd function");
+          return d;
+        },
+        d -> {
+          System.out.println("2nd function");
+          return d;
+        },
+        d -> {
+          System.out.println("function to make a treasure from diamond");
+          return d;
+        });
+    chain(new Diamond(), functions);
+  }
 
+  private Diamond chain(Diamond diamond, List<Function1<Diamond, Diamond>> functions) {
+    return functions.reduceLeft(Function1::andThen).apply(diamond);
   }
 }
