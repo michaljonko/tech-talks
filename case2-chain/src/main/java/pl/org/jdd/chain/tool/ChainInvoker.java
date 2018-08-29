1f4e4f2bdf3332c6498c67817eddb11468fc29f8
Krzysztof Borowa <kborowa@gmail.com>
2018-08-29 23:35:17 +0200

    add simple chain invoker

---
+++ case2-chain/src/main/java/pl/org/jdd/chain/tool/ChainInvoker.java
@@ -0,0 +1,12 @@
+package pl.org.jdd.chain.tool;
+
+import io.vavr.Function1;
+import io.vavr.collection.List;
+import io.vavr.control.Either;
+import pl.org.jdd.legacy.stub.Souvenir;
+
+public class ChainInvoker {
+    public static <R> R invokeChain(Souvenir souvenir, Function1... functions) {
+        return (R) List.of(functions).reduceLeft(Function1::andThen).apply(souvenir);
+    }
+}
